d959e04d7ed2e2edce2939d66dc4b00b26dbe689
Anastasiia Onyshchenko <a.onyshchenko@aimprosoft.com>
2020-10-13 18:29:21 +0300

    hooks: 1-useState

--- src/Hooks/UseState.js
+++ src/Hooks/UseState.js
@@ -1,10 +1,14 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect } from 'react';
 
 
 //создаём хукб который возвращает ключи, которые нужеы для инпут валюе
 function UseStateHook( ) {
     const [count, setCount] = useState(0);
-
+// Аналогично componentDidMount и componentDidUpdate:
+    useEffect(() => {
+        // Обновляем заголовок документа с помощью API браузера
+        document.title = `Вы нажали ${count} раз`;
+    });
     return(
         <div>
             <p>Вы кликнули {count} раз(а)</p>
